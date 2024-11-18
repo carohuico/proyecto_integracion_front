0e9893024fa4883656339dd7ae5408fdc276a46c
ArturoMelchor <Jr-dan-nano@hotmail.com>
2024-11-18 01:54:04 -0600

    Implementación del buscador por id del cliente y validaciones listas

--- app/controllers/historial.js
+++ app/controllers/historial.js
@@ -3,11 +3,15 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class HistoryController extends Controller {
+
   @tracked isModalOpen = false;
   @tracked isDetailModalOpen = false;
   @tracked isEditModalOpen = false;
   @tracked selectedEntry = null;
   @tracked history = [];
+  @tracked searchResults = [];
+  @tracked clienteId = '';
+  @tracked errorMessage = '';
 
  // Cargar historial al iniciar el controlador
   constructor() {
@@ -35,6 +39,64 @@ async loadHistory() {
     console.error('Error loading history:', error);
   }
 }
+
+// Buscador de historial por ID de cliente
+@action
+async searchHistory(clienteId) {
+  try {
+    let response = await fetch(`http://34.31.19.169:5013/api/historial-credito/${clienteId}`);
+    if (!response.ok) {
+      // Si el servidor responde con un error (como 404)
+      if (response.status === 404) {
+        this.searchResults = [];
+        if (clienteId !== null && clienteId.trim() !== '') {
+          // Si el clienteId no es null o vacío, muestra la notificación
+          this.errorMessage = 'No se encontró historial para el cliente con el ID proporcionado.';
+        } else {
+          // Si el clienteId es null o vacío, limpia el mensaje
+          this.errorMessage = '';
+        }
+      } else {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+    } else {
+      let data = await response.json();
+      if (data.length === 0) {
+        // Si el resultado está vacío, no establecer error
+        this.searchResults = [];
+        this.errorMessage = ''; // Limpia el mensaje de error
+      } else {
+        // Si hay resultados, asigna los datos
+        this.searchResults = data.map(entry => ({
+          id: entry.id_credito,
+          estado: entry.estado_credito,
+          pactado: entry.valor_pactado,
+          pagado: entry.valor_pagado,
+          fecha: entry.fecha_pago,
+          monto: entry.monto_pago,
+          clienteId: entry.id_cliente,
+        }));
+        this.errorMessage = ''; // Limpia el mensaje de error si se obtuvieron resultados
+      }
+    }
+  } catch (error) {
+    console.error('Error fetching history:', error);
+    this.errorMessage = 'Hubo un problema al buscar el historial. Intente nuevamente.';
+  }
+}
+
+
+
+  @action
+  updateClienteId(event) {
+    this.clienteId = event.target.value;
+  }
+
+  @action
+  async handleSearch(event) {
+    event.preventDefault();
+    await this.searchHistory(this.clienteId);
+  }
 
   @action
   openModal() {
